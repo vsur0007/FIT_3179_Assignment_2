ab555d492ccb8060986359b29cbf620b6d1ed19d
vsur0007 <126365696+vsur0007@users.noreply.github.com>
2023-10-16 16:50:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,10 @@ var viz_2 = "Histogram_Chart.json";
 var viz_3 = "Scatter_Plot.json";
 var viz_4 = "Heat_Maps.json";
 
-vegaEmbed("#mapncharts", viz_1,{renderer: "svg", actions: false}).then(function (result) {
-    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+vegaEmbed("#mapncharts", viz_1).then(function (result) {}).catch(console.error);
 
-vegaEmbed("#barchart", viz_2,{renderer: "svg", actions: false}).then(function (result) {
-    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+vegaEmbed("#barchart", viz_2).then(function (result) {}).catch(console.error);
 
-vegaEmbed("#plotchart", viz_3,{renderer: "svg", actions: false}).then(function (result) {
-    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+vegaEmbed("#plotchart", viz_3).then(function (result) {}).catch(console.error);
 
-vegaEmbed("#heatmap", viz_4,{renderer: "svg", actions: false}).then(function (result) {
-    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+vegaEmbed("#heatmap", viz_4).then(function (result) {}).catch(console.error);
