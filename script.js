c3b1da5fe88ac1f68882aba43a63081d0adfb36d
vsur0007 <126365696+vsur0007@users.noreply.github.com>
2023-10-16 16:56:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-// script.js
-var viz_1 = "Maps_with_graphs.json";
-var viz_2 = "Histogram_Chart.json";
-var viz_3 = "Scatter_Plot.json";
-var viz_4 = "Heat_Maps.json";
+window.onload = function () {
+  var viz_1 = "Maps_with_graphs.json";
+  var viz_2 = "Histogram_Chart.json";
+  var viz_3 = "Scatter_Plot.json";
+  var viz_4 = "Heat_Maps.json";
 
-vegaEmbed("#mapncharts", viz_1).then(function (result) {}).catch(console.error);
-
-vegaEmbed("#barchart", viz_2).then(function (result) {}).catch(console.error);
-
-vegaEmbed("#plotchart", viz_3).then(function (result) {}).catch(console.error);
-
-vegaEmbed("#heatmap", viz_4).then(function (result) {}).catch(console.error);
+  vegaEmbed("#mapncharts", viz_1).then(function (result) {}).catch(console.error);
+  vegaEmbed("#barchart", viz_2).then(function (result) {}).catch(console.error);
+  vegaEmbed("#plotchart", viz_3).then(function (result) {}).catch(console.error);
+  vegaEmbed("#heatmap", viz_4).then(function (result) {}).catch(console.error);
+};
